08bbf7fe67b01dd04da5e541bf47d09a541c419a
Snowrlax <sonawanepranav19@gmail.com>
2024-04-12 15:35:11 +0530

    added name to function

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,7 @@
 import { NEXT_AUTH } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 
-export default async function () {
+export default async function UserPage () {
     const session = await getServerSession(NEXT_AUTH)
     return (
         <div className="">
